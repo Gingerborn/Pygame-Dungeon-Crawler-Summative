baf82edd68368d1a6c90e8aac0a795dad68a94a3
Miettz <34483768+Miettz@users.noreply.github.com>
2018-01-19 09:55:45 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,272 @@
+#jumpy platform game
+import pygame as pg
+import random
+from settings import *
+from sprites import *
+from map import *
+import sys 
+
+pg.init()
+
+class Game:
+    def __init__(self,folder):
+        #initialize game window
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH,HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(10,100)
+        self.running = True
+        self.folder = folder
+        self.load_data()
+
+    def load_data(self):
+        self.map = Map(map_file)
+        self.wall_img = pg.image.load(wall_image_png).convert_alpha()
+        self.bullet_img = pg.image.load(bullet_image_png).convert_alpha()
+        self.wall_img = pg.transform.scale(self.wall_img,(TILESIZE,TILESIZE))
+        self.bullet_img = pg.transform.scale(self.bullet_img,(TILESIZE,TILESIZE))
+        self.mob_img = pg.image.load(mob_image_png).convert_alpha()
+        self.mob_img = pg.transform.scale(self.mob_img,(TILESIZE,TILESIZE))
+        self.player_img2 = pg.image.load(os.path.join(other_folder,player_img3)).convert_alpha()
+        self.floor_img = pg.image.load(floor_image_png).convert_alpha()
+        self.floor_img = pg.transform.scale(self.floor_img,(TILESIZE,TILESIZE))
+
+    def new(self,folder):
+        # start a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.bullets = pg.sprite.Group()
+        self.floors = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
+        for row,tiles in enumerate(self.map.data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self,col,row)
+                if tile == 'p':
+                    self.player = Player(self.folder,self,col,row)
+                if tile == 'm':
+                    Mob(self,col,row)
+##                    Floor(self,col,row)
+##                if tile == '.':
+##                    Floor(self,col,row)
+                    
+        self.camera = Camera(self.map.width,self.map.height)
+        self.run()
+
+    def run(self):
+        # game loop
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 100
+##            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # game loop - update
+        self.all_sprites.update()
+        self.camera.update(self.player)
+##        pg.display.flip()
+
+    def events(self):
+        # game loops - events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+    def draw(self):
+        pg.display.set_caption("{:.2f}".format(self.clock.get_fps()))
+        #game loop - draw
+        self.screen.fill(BLACK)
+##        self.all_sprites.draw(self.screen)
+        #after drawing everything, flip the display
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image,self.camera.apply(sprite))
+##      This checks hitbox
+##        pg.draw.rect(self.screen,WHITE,self.player.hit_rect,2)
+        pg.display.flip()
+            
+
+    def show_start_screen(self):
+        # game splash/start screen
+        pass
+
+    def show_go_screen(self):
+        # game over/continue
+        pass
+
+def text_objects(text,font,colour):
+    textSurface = font.render(text,True,colour)
+    return textSurface,textSurface.get_rect()
+
+def ninja():
+    folder = ninja_folder
+    g = Game(folder)
+    while g.running:
+        g.new(folder)
+        
+def knight():
+    folder = knight_folder
+    g = Game(folder)
+    while g.running:
+        g.new(folder)
+
+def barbarian():
+    folder = barb_folder
+    g = Game(folder)
+    while g.running:
+        g.new(folder)
+
+def wizard():
+    folder = wizard_folder
+    g = Game(folder)
+    while g.running:
+        g.new(folder)
+
+def warlock():
+    folder = warlock_folder
+    g = Game(folder)
+    while g.running:
+        g.new(folder)
+
+def thief():
+    folder = thief_folder
+    g = Game(folder)
+    while g.running:
+        g.new(folder)
+
+def secret_tank():
+    folder = secret_folder
+    g = Game(folder)
+    while g.running:
+        g.new(folder)
+
+def quitgame():
+    pg.quit()
+    quit()
+    
+def unpause():
+    global pause
+    pg.mixer.music.unpause()
+    pause = False
+
+def paused():
+    global pause
+    pause = True
+    pg.mixer.music.pause()
+    while pause:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+        gameDisplay.fill(BLACK)
+        largeText = pg.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects('Paused',largeText,WHITE)
+        TextRect.center = ((WIDTH/2,HEIGHT/2))
+        gameDisplay.blit(TextSurf,TextRect)
+
+        button("Continue!",WIDTH*0.15,WIDTH*0.75,100,50,GREEN,BRIGHT_GREEN,unpause)
+##        button("Character Select",WIDTH*0.45,WIDTH*0.75,200,50,BLUE,BRIGHT_BLUE,go_back)
+        button("Quit",WIDTH*0.8,WIDTH*0.75,100,50,RED,BRIGHT_RED,quitgame)
+        pg.display.update()
+        clock.tick(15)
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pg.mouse.get_pos()
+    click = pg.mouse.get_pressed()
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pg.draw.rect(gameDisplay,ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action()
+
+    else:
+        pg.draw.rect(gameDisplay,ic,(x,y,w,h))
+
+    smallText = pg.font.Font('freesansbold.ttf',20)
+    textSurf,textRect = text_objects(msg,smallText,BLACK)
+    textRect.center = ((x+(w/2),(y+(h/2))))
+    gameDisplay.blit(textSurf,textRect)
+
+def tank_subclasses():
+    tank_intro = True
+    while tank_intro:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+        gameDisplay.fill(BLACK)
+        button("Knight",WIDTH*0.25,HEIGHT*0.1,WIDTH / 2,HEIGHT / 10,WHITE,GREEN,knight)
+        button("Barbarian",WIDTH*0.25,HEIGHT*0.45,WIDTH / 2,HEIGHT / 10,WHITE,YELLOW,barbarian)
+        button("Quit",WIDTH*0.25,HEIGHT*0.8,WIDTH / 2,HEIGHT / 10,WHITE,BRIGHT_RED,quitgame)
+        pg.display.update()
+        clock.tick(15)
+
+def hunter_subclasses():
+    hunter_intro = True
+    while hunter_intro:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+        gameDisplay.fill(BLACK)
+        button("Thief",WIDTH*0.25,HEIGHT*0.1,WIDTH / 2,HEIGHT / 10,WHITE,RED,thief)
+        button("Ninja",WIDTH*0.25,HEIGHT*0.45,WIDTH / 2,HEIGHT / 10,WHITE,MAGENTA,ninja)
+        button("Quit",WIDTH*0.25,HEIGHT*0.8,WIDTH / 2,HEIGHT / 10,WHITE,BRIGHT_RED,quitgame)
+        pg.display.update()
+        clock.tick(15)
+
+def mage_subclasses():
+    mage_intro = True
+    while mage_intro:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+        gameDisplay.fill(BLACK)
+        button("Wizard",WIDTH*0.25,HEIGHT*0.3,WIDTH / 2,HEIGHT / 10,WHITE,BLUE,wizard)
+        button("Warlock",WIDTH*0.25,HEIGHT*0.6,WIDTH / 2,HEIGHT / 10,WHITE,CYAN,warlock)
+        button("Quit",WIDTH*0.25,HEIGHT*0.8,WIDTH / 2,HEIGHT / 10,WHITE,BRIGHT_RED,quitgame)
+        pg.display.update()
+        clock.tick(15)
+
+def character_select():
+    char_intro = True
+    while char_intro:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+        gameDisplay.fill(BLACK)
+        button("Tank",WIDTH*0.25,HEIGHT*0.25,WIDTH / 2,HEIGHT / 10,WHITE,RED,tank_subclasses)
+        button("Mage",WIDTH*0.25,HEIGHT*0.45,WIDTH / 2,HEIGHT / 10,WHITE,GREEN,mage_subclasses)
+        button("Hunter",WIDTH*0.25,HEIGHT*0.65,WIDTH / 2,HEIGHT / 10,WHITE,BLUE,hunter_subclasses)
+        button("Secret",0,HEIGHT - 50,100,50,BLACK,WHITE,secret_tank)
+        button("Quit",WIDTH*0.25,HEIGHT*0.85,WIDTH / 2,HEIGHT / 10,WHITE,BRIGHT_RED,quitgame)
+        pg.display.update()
+        clock.tick(15)
+
+def game_intro():
+    gam_intro = True
+    while gam_intro:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+        gameDisplay.fill(BLACK)
+        largeText = pg.font.Font('freesansbold.ttf',250)
+        TextSurf, TextRect = text_objects('POWER',largeText,WHITE)
+        TextRect.center = ((WIDTH/2,HEIGHT/2))
+        gameDisplay.blit(TextSurf,TextRect)
+
+        button("Play",WIDTH*0.2,HEIGHT*0.75,100,50,GREEN,BRIGHT_GREEN,character_select)
+        button("Quit",WIDTH*0.75,HEIGHT*0.75,100,50,RED,BRIGHT_RED,quitgame)
+        
+        pg.display.update()
+        clock.tick(15)
+
+game_intro()
+pg.quit()
---
+++ sprites.py
@@ -0,0 +1,254 @@
+#sprite classes for platform
+
+from settings import *
+import pygame as pg
+import os
+import time
+
+from random import uniform
+from map import collide_hit_rect
+
+vec = pg.math.Vector2
+
+
+def collide_with_walls(sprite,group,dir):
+    if dir == 'x':
+        hits = pg.sprite.spritecollide(sprite,group,False,collide_hit_rect)
+        if hits:
+            if sprite.vel.x > 0:
+                sprite.pos.x = hits[0].rect.left - sprite.hit_rect.width/2
+            if sprite.vel.x < 0:
+                sprite.pos.x = hits[0].rect.right + sprite.hit_rect.width/2
+            sprite.vel.x = 0
+            sprite.hit_rect.centerx = sprite.pos.x
+    if dir == 'y':
+        hits = pg.sprite.spritecollide(sprite,group,False,collide_hit_rect)
+        if hits:
+            if sprite.vel.y > 0:
+                sprite.pos.y = hits[0].rect.top - sprite.hit_rect.height/2
+            if sprite.vel.y < 0:
+                sprite.pos.y = hits[0].rect.bottom + sprite.hit_rect.height/2
+            sprite.vel.y = 0
+            sprite.hit_rect.centery = sprite.pos.y
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self,folder,game,x,y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self,self.groups)
+        self.game = game
+        self.image = pg.image.load(os.path.join(folder,'RS.png')).convert()
+        self.image.set_colorkey(WHITE)
+        self.image2 = game.player_img2
+        self.rect = self.image.get_rect()
+        self.hit_rect = player_hit_rect
+        self.hit_rect.center = self.rect.center
+##        self.rect.center = (WIDTH/2,HEIGHT / 2)
+        self.pos = vec(x,y)*TILESIZE
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.frame = 0
+        self.stand_timer = 10
+        self.walk_timer = 0
+        self.last_shot = 0
+        self.folder = folder
+        
+##        self.groups = game.all_sprites
+##        pg.sprite.Sprite.__init__(self,self.groups)
+##        self.game = game
+##        self.rect = self.image.get_rect()
+##        self.hit_rect = player_hit_rect
+##        self.hit_rect.center = self.rect.center
+##        self.vel = vec(0,0)
+##        self.pos = vec(x,y)*tilesize
+##        self.rot = 0
+##        self.last_shot = 0
+
+## STAND TIMER STARTS AT 5 AND WALK TIMER STARTS COUNTING UP BY 1, ONCE IT REACHES 5 STAND TIMER IS SET
+## TO 0 AND THEN IT STARTS GOING UP BY 1, AND THE STATE AT WHICH EITHER TIMER IS IN DICTATES WHICH
+## FRAME WILL BE CALLED, IF STAND TIMER IS AT 5, THEN FRAME 0 OR 3 WILL BE CALLED DEPENDING ON WHICH
+## KEY IS BEING PUSHED DOWN, LEFT OR RIGHT (IT LOOKS LIKE A BUNCH OF TRASH BUT HEY! THE TRASH WORKS!)
+
+    def flip(self):
+        if self.frame == 0:
+            if self.walk_timer >= 10:
+                self.stand_timer = 0
+            self.image = pg.image.load(os.path.join(self.folder,'RS.png')).convert()
+            self.image.set_colorkey(WHITE)
+        if self.frame == 1:
+            if self.walk_timer >= 10:
+                self.stand_timer = 0
+            self.image = pg.image.load(os.path.join(self.folder,'LS.png')).convert()
+            self.image.set_colorkey(WHITE)
+        if self.frame == 2:
+            if self.stand_timer >= 10:
+                self.walk_timer = 0
+            self.image = pg.image.load(os.path.join(self.folder,'RW.png')).convert()
+            self.image.set_colorkey(WHITE)
+        if self.frame == 3:
+            if self.stand_timer >= 10:
+                self.walk_timer = 0
+            self.image = pg.image.load(os.path.join(self.folder,'LW.png')).convert()
+            self.image.set_colorkey(WHITE)
+        if self.frame == 4:
+            if self.walk_timer >= 10:
+                self.stand_timer = 0
+            self.image = pg.image.load(os.path.join(self.folder,'UL.png')).convert()
+            self.image.set_colorkey(WHITE)
+        if self.frame == 5:
+            if self.stand_timer >= 10:
+                self.walk_timer = 0
+            self.image = pg.image.load(os.path.join(self.folder,'UR.png')).convert()
+            self.image.set_colorkey(WHITE)
+        if self.frame == 6:
+            if self.walk_timer >= 10:
+                self.stand_timer = 0
+            self.image = pg.image.load(os.path.join(self.folder,'DL.png')).convert()
+            self.image.set_colorkey(WHITE)
+        if self.frame == 7:
+            if self.stand_timer >= 10:
+                self.walk_timer = 0
+            self.image = pg.image.load(os.path.join(self.folder,'DR.png')).convert()
+            self.image.set_colorkey(WHITE)
+            
+    def update(self):
+        self.flip()
+        self.acc = vec(0,0)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.pos += self.vel * self.game.dt
+        self.hit_rect.centerx = self.pos.x
+        collide_with_walls(self,self.game.walls,'x')
+        self.hit_rect.centery = self.pos.y
+        collide_with_walls(self,self.game.walls,'y')
+        self.rect.center = self.hit_rect.center
+        keys = pg.key.get_pressed()
+        # left
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+            if self.stand_timer >= 10:
+                self.frame = 1
+                self.walk_timer += 1
+            elif self.walk_timer >= 10:
+                self.frame = 3
+                self.stand_timer += 1
+        # right
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+            if self.stand_timer >= 10:
+                self.frame = 0
+                self.walk_timer += 1
+            elif self.walk_timer >= 10:
+                self.frame = 2
+                self.stand_timer += 1
+        #up
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.acc.y = -PLAYER_ACC
+            if self.stand_timer >= 10:
+                self.frame = 4
+                self.walk_timer += 1
+            elif self.walk_timer >= 10:
+                self.frame = 5
+                self.stand_timer += 1
+        #down
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.acc.y = PLAYER_ACC
+            if self.stand_timer >= 10:
+                self.frame = 6
+                self.walk_timer += 1
+            elif self.walk_timer >= 10:
+                self.frame = 7
+                self.stand_timer += 1
+            
+        # wrap around the sides of the screen   
+##        if self.pos.x > WIDTH - 15:
+##            self.acc = vec(0,0)
+##            self.vel = vec(0,0)
+##            self.pos.x -= 0.1
+##        if self.pos.x < 15:
+##            self.acc = vec(0,0)
+##            self.vel = vec(0,0)
+##            self.pos.x += 0.1
+##        if self.pos.y > HEIGHT - 20:
+##            self.acc = vec(0,0)
+##            self.vel = vec(0,0)
+##            self.pos.y -= 0.1
+##        if self.pos.y < 20:
+##            self.acc = vec(0,0)
+##            self.vel = vec(0,0)
+##            self.pos.y += 0.1
+        # apply friction
+        self.acc += self.vel * PLAYER_FRICTION
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        
+        
+      
+
+        self.rect.center = self.pos
+
+
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self,game,x,y):
+        self.groups = game.all_sprites,game.walls
+        pg.sprite.Sprite.__init__(self,self.groups)
+        self.game = game
+        self.image = game.wall_img
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+    
+class Floor(pg.sprite.Sprite):
+    def __init__(self,game,x,y):
+        self.groups = game.all_sprites,game.floors
+        pg.sprite.Sprite.__init__(self,self.groups)
+        self.game = game
+        self.image = game.floor_img
+        self.rect =self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+        
+class Mob(pg.sprite.Sprite):
+    def __init__(self,game,x,y):
+        self.groups = game.all_sprites,game.mobs
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.wall_img
+        self.rect = self.image.get_rect()
+        self.hit_rect = mob_hit_rect.copy()
+        self.hit_rect.center = self.rect.center
+        self.pos = vec(x,y) * TILESIZE
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.rect.center = self.pos
+        self.rot = 0
+        
+    def update(self):
+        self.rot = (self.game.player.pos - self.pos).angle_to(vec(1,0))
+##        print(self.rot)
+        if 45 >= self.rot >= -45:
+            self.image = pg.transform.rotate(self.game.mob_img, 0)
+        if 135 >= self.rot >= 45:
+            self.image = pg.transform.rotate(self.game.mob_img, 90)
+        if self.rot >= 135 or self.rot <= -135:
+            self.image = pg.transform.rotate(self.game.mob_img, 180)
+        if self.rot <= -45 and self.rot >= -135:
+            self.image = pg.transform.rotate(self.game.mob_img, -90)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.acc = vec(mob_speed,0).rotate(-self.rot)
+        self.acc +=self.vel * -1
+        self.vel += self.acc * self.game.dt
+        self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt **2
+        self.hit_rect.centerx = self.pos.x
+        collide_with_walls(self,self.game.walls,'x')
+        self.hit_rect.centery = self.pos.y
+        collide_with_walls(self,self.game.walls,'y')
+        self.rect.center = self.hit_rect.center
+    
